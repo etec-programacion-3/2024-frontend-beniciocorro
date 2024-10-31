1527c9158ef483800c86b383f94319c8c5a31fa6
beniciocorro <b.corro@alumno.etec.um.edu.ar>
2024-10-31 11:57:54 -0300

    Feliz halloween

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
 import smb from './media/SideBar/smb.png';
 import splatoon from './media/SideBar/splatoon.png';
@@ -7,17 +8,65 @@ import ssbu from './media/SideBar/ssbu.png';
 import zelda from './media/SideBar/zelda.png';
 import fireEmblem from './media/SideBar/FireEmblem.png';
 import link from './media/Amiibos/link.jpg';
+import  marina_pearl from './media/Amiibos/marina_pearl.png';
 
 function App() {
+  const [selectedFranchise, setSelectedFranchise] = useState(null);
+
+  const handleIconClick = (franchise) => {
+    setSelectedFranchise(franchise);
+  };
+
+  const renderAmiibos = () => {
+    switch (selectedFranchise) {
+      case 'zelda':
+        return <div className="amiibo-card"><img src={link} alt="Link" /></div>;
+      case 'splatoon':
+        return <div className="amiibo-card"><img src={marina_pearl} alt="Marina_Pearl" /></div>;
+      default:
+        return <p>usted no debería estar viendo este mensaje :3</p>;
+    }
+  };
+
   return (
     <div className="App">
       <div className="sidebar">
-        <img src={smb} alt="SMB" />
-        <img src={splatoon} alt="Splatoon" />
-        <img src={acnh} alt="Leaf" />
-        <img src={ssbu} alt="Smash" />
-        <img src={zelda} alt="Triforce" />
-        <img src={fireEmblem} alt="Sword" />
+        <img 
+          src={smb} 
+          alt="SMB" 
+          onClick={() => handleIconClick('smb')}
+          className={selectedFranchise === 'smb' ? 'selected' : ''}
+        />
+        <img 
+          src={splatoon} 
+          alt="Splatoon" 
+          onClick={() => handleIconClick('splatoon')}
+          className={selectedFranchise === 'splatoon' ? 'selected' : ''}
+        />
+        <img 
+          src={acnh} 
+          alt="Leaf" 
+          onClick={() => handleIconClick('acnh')}
+          className={selectedFranchise === 'acnh' ? 'selected' : ''}
+        />
+        <img 
+          src={ssbu} 
+          alt="Smash" 
+          onClick={() => handleIconClick('ssbu')}
+          className={selectedFranchise === 'ssbu' ? 'selected' : ''}
+        />
+        <img 
+          src={zelda} 
+          alt="Triforce" 
+          onClick={() => handleIconClick('zelda')}
+          className={selectedFranchise === 'zelda' ? 'selected' : ''}
+        />
+        <img 
+          src={fireEmblem} 
+          alt="Sword" 
+          onClick={() => handleIconClick('fireEmblem')}
+          className={selectedFranchise === 'fireEmblem' ? 'selected' : ''}
+        />
       </div>
       
       <div className="main-content">
@@ -25,9 +74,7 @@ function App() {
           <input type="text" placeholder="Buscar..." />
         </div>
         <div className="amiibo-container">
-          <div className="amiibo-card">
-            <img src={link} alt="Link" />
-          </div>
+          {renderAmiibos()}
         </div>
       </div>
     </div>
