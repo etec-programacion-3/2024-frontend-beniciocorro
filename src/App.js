b48ca35f94079afded038342802f134221212be8
beniciocorro <b.corro@alumno.etec.um.edu.ar>
2024-11-06 11:50:22 -0300

    Actualizado Productos

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import ssbu from './media/SideBar/ssbu.png';
 import zelda from './media/SideBar/zelda.png';
 import fireEmblem from './media/SideBar/FireEmblem.png';
 import link from './media/Amiibos/link.jpg';
-import  marina_pearl from './media/Amiibos/marina_pearl.png';
+import  marina from './media/Amiibos/marina.jpg';
+import isabelle from './media/Amiibos/isabelle-ssbu.png';
+import mario from './media/Amiibos/mario.png';
+import kazuya from './media/Amiibos/kazuya.png';
+import chrom from './media/Amiibos/chrom.png';
 
 function App() {
   const [selectedFranchise, setSelectedFranchise] = useState(null);
@@ -19,14 +23,58 @@ function App() {
 
   const renderAmiibos = () => {
     switch (selectedFranchise) {
-      case 'zelda':
-        return <div className="amiibo-card"><img src={link} alt="Link" /></div>;
+      case 'smb':
+        return (
+          <div className="amiibo-card">
+            <img src={mario} alt="Mario" />
+            <p className="price">$12.99</p>
+            <button className="buy-button">Comprar</button>
+          </div>
+        );
       case 'splatoon':
-        return <div className="amiibo-card"><img src={marina_pearl} alt="Marina_Pearl" /></div>;
+        return (
+          <div className="amiibo-card">
+            <img src={marina} alt="Marina" />
+            <p className="price">$15.99</p>
+            <button className="buy-button">Comprar</button>
+          </div>
+        );
+      case 'acnh':
+        return (
+          <div className="amiibo-card">
+            <img src={isabelle} alt="Isabelle" />
+            <p className="price">$9.99</p>
+            <button className="buy-button">Comprar</button>
+          </div>
+        );
+      case 'ssbu':
+        return (
+          <div className="amiibo-card">
+            <img src={kazuya} alt="Kazuya" />
+            <p className="price">$14.99</p>
+            <button className="buy-button">Comprar</button>
+          </div>
+        );
+      case 'zelda':
+        return (
+          <div className="amiibo-card">
+            <img src={link} alt="Link" />
+            <p className="price">$13.99</p>
+            <button className="buy-button">Comprar</button>
+          </div>
+        );
+      case 'fireEmblem':
+        return (
+          <div className="amiibo-card">
+            <img src={chrom} alt="Chrom" />
+            <p className="price">$16.99</p>
+            <button className="buy-button">Comprar</button>
+          </div>
+        );
       default:
         return <p>usted no debería estar viendo este mensaje :3</p>;
     }
-  };
+  };  
 
   return (
     <div className="App">
